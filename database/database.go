45d8386f499bc5f746a7b0bcf130748724b62095
Nikita <109899745+vvetta@users.noreply.github.com>
2025-08-16 13:57:14 +0300

    create 2 crud funcs

--- database/database.go
+++ database/database.go
@@ -2,7 +2,8 @@ package database
 
 
 import (
-	
+	"fmt"	
+
 	"WB-Tech-L0/models"
   "WB-Tech-L0/config"
 
@@ -32,16 +33,40 @@ func InitDB() error {
 }
 
 
-func AddOrderToDB(order models.Order) error {
+func AddOrderToDB(order *models.Order) error {
 // Сохраняет заказ в базу данных.
+	if order == nil {
+		return fmt.Errorf("Нулевой указатель заказа!")
+	}
 
-	return nil
+	err := DB.Transaction(func(tx *gorm.DB) error {
+		err := tx.Create(order).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return err
 }
 
 
 func GetOrdersFromDB(orders_uid []string) ([]models.Order, error) {
 // Возвращает заказы из базы данных.
+	if len(orders_uid) == 0 {
+		return nil, fmt.Errorf("Пустой список id заказов!")
+	}
+
 	var orders []models.Order
+
+	err := DB.Preload("Items").Preload("Delivery").Preload("Payment").Where("order_uid IN ?", orders_uid).Find(&orders).Error
+	if err != nil {
+		return nil, fmt.Errorf("Ошибка при получении заказов: %v", err)
+	}
+
+	if len(orders) == 0 {
+		return nil, fmt.Errorf("Заказы не найдены!")
+	}
 
 	return orders, nil
 }
