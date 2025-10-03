9adc3e0a41879d2612be549ed84d0a38406b8da4
Nikita <109899745+vvetta@users.noreply.github.com>
2025-10-03 12:43:16 +0300

    Added a separate model for the database and translation functions

---
+++ internal/adapters/repository/mapper.go
@@ -0,0 +1,122 @@
+package repository
+
+import (
+	"WB-Tech-L0/internal/domain"
+)
+
+func toDomainOrder(order *Order) *domain.Order {
+// Преобразует GORM модель в доменную.
+	o := &domain.Order{
+		OrderUID: order.OrderUID,
+		TrackNumber: order.TrackNumber,
+		Entry: order.Entry,
+		Delivery: domain.DeliveryInfo{
+			Name: order.Delivery.Name,
+			Phone: order.Delivery.Phone,
+			Zip: order.Delivery.Zip,
+			City: order.Delivery.City,
+			Address: order.Delivery.Address,
+			Region: order.Delivery.Region,
+			Email: order.Delivery.Email,
+		},
+		Payment: domain.PaymentInfo{
+			Transaction: order.Payment.Transaction,
+			RequestID: order.Payment.RequestID,
+			Currency: order.Payment.Currency,
+			Provider: order.Payment.Provider,
+			Amount: order.Payment.Amount,
+			PaymentDT: order.Payment.PaymentDT,
+			Bank: order.Payment.Bank,
+			DeliveryCost: order.Payment.DeliveryCost,
+			GoodsTotal: order.Payment.GoodsTotal,
+			CustomFee: order.Payment.CustomFee,
+		},
+		Locale: order.Locale,
+		InternalSignature: order.InternalSignature,
+		CustomerID: order.CustomerID,
+		DeliveryService: order.DeliveryService,
+		ShardKey: order.ShardKey,
+		SMID: order.SMID,
+		DateCreated: order.DateCreated,
+		OOFShard: order.OOFShard,
+	}
+
+	o.Items = make([]domain.ItemInfo, 0, len(order.Items))
+	for _, item := range order.Items {
+		o.Items = append(o.Items, domain.ItemInfo{
+			OrderUID: item.OrderUID,
+			ChrtID: item.ChrtID,
+			TrackNumber: item.TrackNumber,
+			Price: item.Price,
+			RID: item.RID,
+			Name: item.Name,
+			Sale: item.Sale,
+			TotalPrice: item.TotalPrice,
+			NmID: item.NmID,
+			Brand: item.Brand,
+			Status: item.Status,
+		})
+	}
+
+	return o
+}
+
+func toGORMOrder(order *domain.Order) *Order {
+// Преобразует доменную модель в GORM.
+	
+	o := &Order{	
+		OrderUID: order.OrderUID,
+		TrackNumber: order.TrackNumber,
+		Entry: order.Entry,
+		Delivery: DeliveryInfo{
+			Name: order.Delivery.Name,
+			Phone: order.Delivery.Phone,
+			Zip: order.Delivery.Zip,
+			City: order.Delivery.City,
+			Address: order.Delivery.Address,
+			Region: order.Delivery.Region,
+			Email: order.Delivery.Email,
+		},
+		Payment: PaymentInfo{
+			Transaction: order.Payment.Transaction,
+			RequestID: order.Payment.RequestID,
+			Currency: order.Payment.Currency,
+			Provider: order.Payment.Provider,
+			Amount: order.Payment.Amount,
+			PaymentDT: order.Payment.PaymentDT,
+			Bank: order.Payment.Bank,
+			DeliveryCost: order.Payment.DeliveryCost,
+			GoodsTotal: order.Payment.GoodsTotal,
+			CustomFee: order.Payment.CustomFee,
+		},
+		Locale: order.Locale,
+		InternalSignature: order.InternalSignature,
+		CustomerID: order.CustomerID,
+		DeliveryService: order.DeliveryService,
+		ShardKey: order.ShardKey,
+		SMID: order.SMID,
+		DateCreated: order.DateCreated,
+		OOFShard: order.OOFShard,
+	}
+
+	o.Items = make([]ItemInfo, 0, len(order.Items))
+	for _, item := range order.Items {
+		o.Items = append(o.Items, ItemInfo{
+			OrderUID: item.OrderUID,
+			ChrtID: item.ChrtID,
+			TrackNumber: item.TrackNumber,
+			Price: item.Price,
+			RID: item.RID,
+			Name: item.Name,
+			Sale: item.Sale,
+			Size: item.Size,
+			TotalPrice: item.TotalPrice,
+			NmID: item.NmID,
+			Brand: item.Brand,
+			Status: item.Status,
+		})
+	}
+
+	return o
+}
+
